7894b9fe9bd4bfa9fedf3a4f3a0ac4d258d332d7
mariesonko <mariesonko62@gmail.com>
2018-05-30 19:00:48 -0400

    Created new form and params

--- app/controllers/hangouts_controller.rb
+++ app/controllers/hangouts_controller.rb
@@ -13,7 +13,15 @@ class HangoutsController < ApplicationController
   end
 
   def create
+    @hangout= Hangout.new(hangout_params)
+    @user = User.find_or_create_by(name: params[:user][:name])
+    @hangout.user = @user
 
+    if @hangout.save
+      redirect_to user_path(@user)
+    else
+      render 'new'
+    end
   end
 
   def edit
@@ -24,5 +32,11 @@ class HangoutsController < ApplicationController
       @hangout = Hangout.find(params[:id])
       redirect_to user_path(session[:current_user_id])
   end
+
+  private
+  def hangout_params
+  params.require(:hangout).permit(:date, :time, :restaurant_id)
+  end
+
 
 end
--- app/models/hangout.rb
+++ app/models/hangout.rb
@@ -1,7 +1,11 @@
 class Hangout < ApplicationRecord
   belongs_to :guest, class_name: "User"
   belongs_to :host, class_name: "User"
-  belongs_to :restaurant 
+  belongs_to :restaurant
   validates :guest_id, presence: true
   validates :host_id, presence: true
+
+  # def host_name=(name)
+  #   self.host = User.find_or_create_by(name)
+  # end
 end
