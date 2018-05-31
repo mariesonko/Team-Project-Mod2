cfef2758a75c1b6bf17663482133c68eae1df16e
jessebillard <jessebillard@mac.com>
2018-05-30 22:34:40 -0400

    Creates sign up form and user create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def show
@@ -7,7 +8,13 @@ class UsersController < ApplicationController
   end
 
   def create
-    
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
+
   end
 
   def edit
@@ -21,4 +28,10 @@ class UsersController < ApplicationController
   def destroy
 
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end 
 end
