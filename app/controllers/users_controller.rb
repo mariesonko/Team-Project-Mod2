511076fde569a6c6f43e0d316e8110d0c723f970
jessebillard <jessebillard@mac.com>
2018-05-31 08:55:30 -0400

    Adds Logout and SessionsHelper

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in(@user)
       redirect_to user_path(@user)
     else
       render :new
@@ -26,7 +27,7 @@ class UsersController < ApplicationController
   end
 
   def destroy
-
+  
   end
 
   private
